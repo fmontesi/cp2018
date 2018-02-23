77dbda1bdf1133793a5b8853622c847086c3e552
fmontesi <famontesi@gmail.com>
2018-02-23 15:13:28 +0100

    Week 8.

--- exercises/src/cp/week8/Exercise5.java
+++ exercises/src/cp/week8/Exercise5.java
@@ -12,5 +12,7 @@ public class Exercise5
 	- Create a new thread-safe class Point, which stores two Counter objects.
 	- The two counter objects should be public.
 	- Implement the method boolean areEqual() in Point, which returns true if the two counters store the same value.
+	- Question: Is the code you obtained robust with respect to client-side locking (see book)?
+				Would it help if the counters were private?
 	*/
 }
